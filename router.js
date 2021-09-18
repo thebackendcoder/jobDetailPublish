507f124a84fd7947bfd582b73e93be6c8aba0d12
Kashif Kalam <Kashif.Kalam@viacom18.com>
2021-09-18 16:31:45 +0530

    added the base route

--- router.js
+++ router.js
@@ -5,14 +5,20 @@ const postJob = require('./controllers/postJob');
 const applyJob = require('./controllers/applyJobs');
 const myPostedRefral = require('./controllers/myPostedRefral');
 const appliedRefrel = require('./controllers/myAppliedRefral');
-const getAppliedCandidate =  require('./controllers/getAllCanditate');
-const shortlistProfile = require('./controllers/shortlistProfile')
+const getAppliedCandidate = require('./controllers/getAllCanditate');
+const shortlistProfile = require('./controllers/shortlistProfile');
 
+async function baseRoute(req, res) {
+    res.status(200).json({
+        message: "base route working"
+    })
+}
+router.get('/', baseRoute);
 router.post('/postJob', postJob);
 router.post('/applyJob', applyJob);
 router.post('/getMyPostedRefferal', myPostedRefral);
-router.post('/appliedRefral',appliedRefrel);
-router.post('/getAllCandidate',getAppliedCandidate);
+router.post('/appliedRefral', appliedRefrel);
+router.post('/getAllCandidate', getAppliedCandidate);
 router.post('/shortlistProfile', shortlistProfile);
 
 
